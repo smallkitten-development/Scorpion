3fcaa859a3adaa539198fcbf1ba4c8c719f016db
Nathan Goodwin <nathanaccidentally@gmail.com>
2019-05-03 14:12:41 -0500

    created development branch

--- scorpion.js
+++ scorpion.js
@@ -1,5 +1,7 @@
 // Scorpion game engine! Made by nathan.
 
+console.log('scorpion-engine 1.0');
+
 function spnLoadThree(location) { // loads three.js into the current page
 	three = document.createElement("script");
 	three.src = location;
@@ -9,10 +11,12 @@ function spnLoadThree(location) { // loads three.js into the current page
 // ^^ loads three too late
 
 function spnScene(alias, fov, x, y, z) {
-	spnCreateScene = new THREE.Scene();
+	window.spnCreateScene = new THREE.Scene();
 	var WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
 
-	console.warn('spnRenderer scene antialias set to ' + alias);
+	if (alias == false) {
+		console.warn('spnRenderer scene antialias set to ' + alias + ". Please check your code to make sure it's correct.");
+	}
 
 	spnRenderer = new THREE.WebGLRenderer({
 		antialias: alias
@@ -25,4 +29,20 @@ function spnScene(alias, fov, x, y, z) {
 	spnCamera = new THREE.PerspectiveCamera(fov, WIDTH / HEIGHT, 2000);
 	spnCamera.position.set(x, y, z);
 	spnCreateScene.add(spnCamera);
+}
+
+function spnBasicCube(material, clr, l, w, depth, x, y, z) { // draw a basic cube and render it
+	if (material == "basic") {
+		var spnBasicCube = new THREE.Mesh(new THREE.CubeGeometry(l, w, depth), new THREE.MeshBasicMaterial({color: clr}));
+		spnBasicCube.position.x = x;
+		spnBasicCube.position.y = y;
+		spnBasicCube.position.z = z;
+		spnCreateScene.add(spnBasicCube);
+
+		spnRenderer.render(spnBasicCube);
+
+		console.log(spnBasicCube);
+
+		// this should end up making a cube but throws an error rn
+	}
 }
