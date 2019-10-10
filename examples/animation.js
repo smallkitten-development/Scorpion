3c24e5043a858f8ffeb2ebdce36f6e0a126c4b60
Nathan Goodwin <nathanaccidentally@gmail.com>
2019-10-10 13:38:32 -0500

    Removed visual stuff and added spnGrid()

--- examples/animation.js
+++ examples/animation.js
@@ -2,7 +2,7 @@
 
 // SCENE SETUP:
 
-spnScene(80, 0, 0, 0, black); // create a scene with a renderer and camera
+spnScene(true, true, 80, 0, 0, 0, black); // create a scene with a renderer and camera
 
 // OBJECT SETUP:
 
---
+++ examples/gridTest.js
@@ -0,0 +1,9 @@
+//test for the grid system
+
+spnScene(80, 0, 2.5, 5);
+
+spnCube('basic', white, 1, 1, 1, 0, 0, 0, false, 'Cube');
+
+spnCube('basic', white, 1, 1, 1, 0, 2, 0, true, 'Cube');
+
+// spnGrid(globalCube.geometry.parameters.width * 2, globalCube.geometry.parameters.width * 2, globalCube.geometry.parameters.width / 2, -globalCube.geometry.parameters.height / 2, globalCube.position.z + globalCube.geometry.parameters.depth / 2);
